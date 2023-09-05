713482f09b4e01da9fa1dc46941b1a45b9faf1c3
BUI-VAN-DUY\buiva <vanduy2992004@gmail.com>
2023-09-05 21:02:58 +0700

    kiemtraweb

---
+++ src/main/java/com/example/kiemtraWeb/KiemtraWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiemtraWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiemtraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiemtraWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiemtraWeb/KiemtraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiemtraWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiemtraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
